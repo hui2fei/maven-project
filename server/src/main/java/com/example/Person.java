46ff68563b2ef1311e6d8718c9cccd05e5938614
tester <teynet@126.com>
2020-03-03 04:45:47 +0800

    fix checkstyle warning

--- server/src/main/java/com/example/Person.java
+++ server/src/main/java/com/example/Person.java
@@ -12,8 +12,11 @@ public class Person {
 
   }
 
-  //TODO: Add javadoc comment
-  public String person(String someone) {
+  /**
+   * @param someone is name
+   * @return is character string
+   */ 
+  public final String person(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
