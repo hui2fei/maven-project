18a0c2269457dc658391cfb761b561505150c736
tester <teynet@126.com>
2020-03-02 21:40:53 +0800

    del space

--- server/src/main/java/com/example/Person.java
+++ server/src/main/java/com/example/Person.java
@@ -15,7 +15,7 @@ public class Person {
   /**
    * @param someone is name
    * @return is character string
-   */ 
+   */
   public final String person(final String someone) {
     return String.format("Hello, %s!", someone);
   }
